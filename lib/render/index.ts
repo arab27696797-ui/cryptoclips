ac12d650d4abcc14bd7a3bc3c67ce46dcb0cb56e
Artur <arab27696797@gmail.com>
2026-04-20 20:18:29 +0300

    Create index.ts

---
+++ lib/render/index.ts
@@ -0,0 +1,5 @@
+export { getTemplate, listTemplates, TEMPLATES } from './templates'
+export { buildFFmpegCommand, computeDuration } from './composition'
+
+export type { TemplateConfig } from './templates'
+export type { CompositionParams, CompositionScene } from './composition'
