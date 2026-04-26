1cedc1229c1254babd571872745c1277e4f0b6cd
Artur <arab27696797@gmail.com>
2026-04-26 22:45:42 +0300

    Update index.ts

--- lib/render/index.ts
+++ lib/render/index.ts
@@ -1,6 +1,7 @@
 export { getTemplate, listTemplates, TEMPLATES } from './templates'
 export { buildFFmpegCommand, computeDuration } from './composition'
 export { enqueueRenderJob, getRenderJobStatus, listRenderJobs } from './queue'
+export { renderVideo } from './renderer'
 
 export type { TemplateConfig } from './templates'
 export type { CompositionParams, CompositionScene } from './composition'
