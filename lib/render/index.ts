2315fc58dcff4b74751087d6d2ee008c40914b46
Artur <arab27696797@gmail.com>
2026-05-07 15:42:34 +0300

    Update index.ts

--- lib/render/index.ts
+++ lib/render/index.ts
@@ -1,9 +1,4 @@
-export { getTemplate, listTemplates, TEMPLATES } from './templates'
-export { buildFFmpegCommand, computeDuration } from './composition'
-export { enqueueRenderJob, getRenderJobStatus, listRenderJobs } from './queue'
-export { renderVideo } from './renderer'
+import { renderVideo as renderVideoNew, RenderVideoParams, RenderVideoResult } from './renderer'
 
-export type { TemplateConfig } from './templates'
-export type { CompositionParams, CompositionScene } from './composition'
-export type { QueueJobStatus } from './queue'
-export type { RenderVideoParams, RenderVideoResult } from './renderer'
+export { renderVideoNew as renderVideo }
+export type { RenderVideoParams, RenderVideoResult
